5692a82d31d56a813296988247c93788c0930bbc
Ezra Mizrahi <32469862+ezramizrahi@users.noreply.github.com>
2020-11-02 09:02:37 +1100

    updated

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+// removes hyphens and adds capitalization
+// e.g. 'vermilion-city' => 'Vermilion City'
+export const formatText = (text) => {
+  const removeHyphens = text.replace(/-/g, ' ')
+  .split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+  .join(' ');
+  return removeHyphens;
+};
+
+// get random item from array
+export const getRandomItemFromArray = (array) => {
+  return array[Math.floor((Math.random()*array.length))];
+};
